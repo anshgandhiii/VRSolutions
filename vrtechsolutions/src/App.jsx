b5f95c584b78d5290154997df481301a8b4bcf28
Dip <dinnjain02@gmail.com>
2025-04-05 16:12:05 +0530

    setup

---
+++ vrtechsolutions/src/App.jsx
@@ -0,0 +1,11 @@
+import Layout from './components/Layout';
+
+function App() {
+  return (
+    <Layout>
+      <h1 className="text-3xl font-bold p-4">Welcome to My App</h1>
+    </Layout>
+  );
+}
+
+export default App;
